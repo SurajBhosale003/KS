fabc87175b37fc1ff1f85330045d5241458d9085
SurajBhosale003 <surajedu003@gmail.com>
2024-10-10 18:16:57 +0530

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',  // Add this line for your TSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
